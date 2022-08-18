521e2323b4169188414f1a42d2cb3a87455d0575
maulanafanny <maulanafanny38@gmail.com>
2022-08-19 00:59:36 +0700

    added goBack feature

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,6 @@ class WebViewExampleState extends State<WebViewExample> {
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
-  Future<bool> _onWillPop() async {
-    return (await showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: const Text('Are you sure?'),
-            content: const Text('Do you want to exit an App'),
-            actions: <Widget>[
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(false),
-                child: const Text('No'),
-              ),
-              TextButton(
-                onPressed: () => Navigator.of(context).pop(true),
-                child: const Text('Yes'),
-              ),
-            ],
-          ),
-        )) ??
-        false;
-  }
-
   int _selectedIndex = 0;
 
   void _onItemTapped(int index) {
@@ -78,46 +57,53 @@ class WebViewExampleState extends State<WebViewExample> {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: _onWillPop,
-      child: MaterialApp(
-        title: 'Test App Webview',
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Maulanafanny GitHub Profile'),
-            backgroundColor: Colors.red,
-          ),
-          body: WebView(
+    return MaterialApp(
+      title: 'Test App Webview',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maulanafanny GitHub Profile'),
+          backgroundColor: Colors.red,
+        ),
+        body: WillPopScope(
+          onWillPop: () async {
+            if (await controller.canGoBack()) {
+              controller.goBack();
+              return false;
+            } else {
+              return true;
+            }
+          },
+          child: WebView(
             initialUrl: 'https://github.com/maulanafanny',
             javascriptMode: JavascriptMode.unrestricted,
             onWebViewCreated: (controller) {
               this.controller = controller;
             },
           ),
-          bottomNavigationBar: BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.book),
-                label: 'Repository',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.star),
-                label: 'Favorites',
-              ),
-            ],
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-          ),
-          drawer: Drawer(
-            backgroundColor: Colors.white,
-            child: Container(
-              padding: const EdgeInsets.fromLTRB(30, 70, 0, 0),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Repository',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: 'Favorites',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(30, 70, 0, 0),
           ),
         ),
       ),
