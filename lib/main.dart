2ec0da2956b87ace99d2d6f766e43380c106485c
maulanafanny <maulanafanny38@gmail.com>
2022-08-18 23:53:49 +0700

    added bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'dart:io';
 
 import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  runApp(WebViewExample());
+  runApp(const WebViewExample());
 }
 
-// ignore: use_key_in_widget_constructors
 class WebViewExample extends StatefulWidget {
+  const WebViewExample({Key? key}) : super(key: key);
+
   @override
   WebViewExampleState createState() => WebViewExampleState();
 }
 
 class WebViewExampleState extends State<WebViewExample> {
+  late WebViewController controller;
+
   @override
   void initState() {
     super.initState();
@@ -21,18 +26,98 @@ class WebViewExampleState extends State<WebViewExample> {
     if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text('Do you want to exit an App'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    switch (index) {
+      case 0:
+        setState(() {
+          _selectedIndex = index;
+          log(_selectedIndex.toString());
+          controller.loadUrl('https://github.com/maulanafanny');
+        });
+        break;
+      case 1:
+        setState(() {
+          _selectedIndex = index;
+          log(_selectedIndex.toString());
+          controller
+              .loadUrl('https://github.com/maulanafanny?tab=repositories');
+        });
+        break;
+      case 2:
+        setState(() {
+          _selectedIndex = index;
+          log(_selectedIndex.toString());
+          controller.loadUrl('https://github.com/maulanafanny?tab=stars');
+        });
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test App Webview',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Test Webview'),
-          backgroundColor: Colors.red,
-        ),
-        body: const WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: 'https://github.com/maulanafanny',
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: MaterialApp(
+        title: 'Test App Webview',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Maulanafanny GitHub Profile'),
+            backgroundColor: Colors.red,
+          ),
+          body: WebView(
+            initialUrl: 'https://github.com/maulanafanny',
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              this.controller = controller;
+            },
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Repository',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.star),
+                label: 'Favorites',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
+          drawer: Drawer(
+            backgroundColor: Colors.white,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(30, 70, 0, 0),
+            ),
+          ),
         ),
       ),
     );
